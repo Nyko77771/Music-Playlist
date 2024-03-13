2671109c262ffe4184049918e665066189428847
Nyko77771 <nykytamcdonald@gmail.com>
2024-03-13 19:24:41 +0000

    upload

---
+++ Song Playlists/src/Node.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nykytamcdonald
+ */
+public class Node {
+    
+    private Song song;
+    private Node prev;
+    private Node next;
+    
+    public Node(Song song, Node next, Node prev){
+        this.song = song;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+    
+}
