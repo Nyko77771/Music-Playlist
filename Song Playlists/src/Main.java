0c0671a1a617f095c277ab8c716194b64b3ae537
Nyko77771 <nykytamcdonald@gmail.com>
2024-03-14 21:51:43 +0000

    changes 2

--- Song Playlists/src/Main.java
+++ Song Playlists/src/Main.java
@@ -6,4 +6,21 @@
  */
 public class Main {
     
+    public static void main(String[] args) {
+        
+    LikedSongPlaylists likedPlaylist = new LikedSongPlaylists();
+    
+    Song song1 = new Song("Don't Worry Be Happy");
+    Song song2 = new Song("Imagine");
+    Song song3 = new Song("Thriller");
+    Song song4 = new Song("Somebody that I used to know");
+    
+    likedPlaylist.push(song4);
+    likedPlaylist.push(song3);
+    likedPlaylist.push(song2);
+    likedPlaylist.push(song1);
+
+    
+    }
+    
 }
--- Song Playlists/src/Song.java
+++ Song Playlists/src/Song.java
@@ -10,31 +10,31 @@
 public class Song {
     
     private String name;
-    private int length;
+    private String length;
     private String artist;
     private String album;
     
-    public Song(String name, int length, String artist, String album){
-        this.name = name;
+    public Song(String nameOfSong, String length, String artist, String album){
+        name = nameOfSong;
         this.length = length;
         this.artist = artist;
         this.album = album;     
     }
     
-    public Song(String name, int length, String artist){
+    public Song(String name, String length, String artist){
         this(name, length, artist, "Unknown");
     }
     
-    public Song(String name, int length){
+    public Song(String name, String length){
         this(name, length, "Unknown", "Unknown");
+    }
+    
+    public Song(String name){
+        this(name, "0:00", "Unknown", "Unknown");
     }
 
     public String getName() {
         return name;
-    }
-
-    public int getLength() {
-        return length;
     }
     
 }
