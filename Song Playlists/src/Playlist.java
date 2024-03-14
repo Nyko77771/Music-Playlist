bff88799eba3856399b6a5a3e427067361c6a9fb
Nykyta <60669163+Nyko77771@users.noreply.github.com>
2024-03-14 16:09:19 +0000

    Add files via upload
    Version 2

--- Song Playlists/src/Playlist.java
+++ Song Playlists/src/Playlist.java
@@ -10,18 +10,10 @@ import java.util.List;
  */
 public abstract class Playlist {
     
-    abstract Song addSong(Object item);
-    abstract Song removeSong();
-    abstract String searchSong(String name);
+    abstract String searchSong(Song song);
     abstract String printPlaylist();
     abstract int size();
     abstract boolean isEmpty();
-
-    @Override
-    public String toString() {
-        return "Playlist{" + '}';
-    }
-    
-    
-    
+    abstract String repeat(boolean answer);
+    abstract void move(int songIndex, int moveIndex);
 }
